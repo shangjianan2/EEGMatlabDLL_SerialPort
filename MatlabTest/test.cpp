a89d6386a102d4c89242625c3cca7dfd30c329db
unknown <1978707987@qq.com>
2018-03-17 10:18:26 +0800

    发现上个版本的bug,由于没有发送开始信号,所以mexw64库并不能控制下位机开始通信 以修改上述bug 新增设备检查功能,库运行时首先先检测此时的usb设备是否已经开启, 如果没有开启,就开启usb,并发送开始信号 如果已经开启,就直接进行通信

--- MatlabTest/test.cpp
+++ MatlabTest/test.cpp
@@ -6,18 +6,50 @@
 
 
 using namespace std;
+
+
+void begin_command(CCyUSBDevice *USBDevice_tt)
+{
+	UCHAR array_begin[2];
+	PUCHAR p_array_begin = array_begin;
+	LONG len_begin;
+	array_begin[0] = 'S';
+	array_begin[1] = 'S';
+	len_begin = 2;
+	if (USBDevice_tt->BulkOutEndPt->XferData(p_array_begin, len_begin) == true){
+		mexPrintf("begin is send\n");
+	}
+	else{
+		mexPrintf("begin is not send\n");
+	}
+}
+
 extern "C" MEX_FUNCTION_API void mexFunction(int nlhs, mxArray *plhs[], int nrhs, mxArray*prhs[])
 {
 	CCyUSBDevice *USBDevice = new CCyUSBDevice();
 	PUCHAR buf = new UCHAR[1024];
 	double *x;
+	
+	
 	plhs[0] = mxCreateDoubleMatrix(1, 1024, mxREAL);
 	x = mxGetPr(plhs[0]);
-	USBDevice->Open(0);
+
+
+	if (USBDevice->IsOpen()){
+		mexPrintf("USB has already been open\n");
+	}
+	else{
+		mexPrintf("USB has not  been open yet\n");
+		USBDevice->Open(0);
+		begin_command(USBDevice);
+	}
+
+	
+
 	if (USBDevice->IsOpen()){
 		LONG len = 1024;
+		
 		USBDevice->BulkInEndPt->XferData(buf, len);
-		mexPrintf("USB is open\n");
 		for (int i = 0; i < 1024; i++){
 			x[i] = buf[i];
 		}
