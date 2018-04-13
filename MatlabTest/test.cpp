d05bdb29872a374b12608d389ac88105d15e6f70
unknown <1978707987@qq.com>
2018-04-13 22:18:24 +0800

    此版本的函数带有输入参数，通过输入参数设定放大倍数

--- MatlabTest/test.cpp
+++ MatlabTest/test.cpp
@@ -8,23 +8,62 @@
 using namespace std;
 
 
-void begin_command(CCyUSBDevice *USBDevice_tt)
+void send_command(CCyUSBDevice *USBDevice_tt, int BeiShu)
 {
 	UCHAR array_begin[6];
 	PUCHAR p_array_begin = array_begin;
 	LONG len_begin;
 	array_begin[0] = 'T';
 	array_begin[1] = 'S';
-	array_begin[2] = 219;
 	
-	array_begin[4] = 182;
-	array_begin[5] = 109;
+	switch (BeiShu)
+	{
+		case 1:
+			array_begin[2] = 0;
+			array_begin[5] = 0;
+			array_begin[4] = 0;
+			break;
+		case 2:
+			array_begin[2] = 36;
+			array_begin[5] = 146;
+			array_begin[4] = 73;
+			
+			break;
+		case 4:
+			array_begin[2] = 73;
+			array_begin[5] = 36;
+			array_begin[4] = 146;
+			
+			break;
+		case 6:
+			array_begin[2] = 109;
+			array_begin[5] = 182;
+			array_begin[4] = 219;
+			
+			break;
+		case 8:
+			array_begin[2] = 146;
+			array_begin[5] = 73;
+			array_begin[4] = 36;
+			break;
+		case 12:
+			array_begin[2] = 182;
+			array_begin[5] = 219;
+			array_begin[4] = 109;
+			break;
+		case 24:
+			array_begin[2] = 219;
+			array_begin[5] = 109;
+			array_begin[4] = 182;
+			break;
+		
+	}
 	
 	len_begin = 6;
 
 	array_begin[3] = 'G';
 	if (USBDevice_tt->BulkOutEndPt->XferData(p_array_begin, len_begin) == true){
-		mexPrintf("FangDa is send G\n");
+		//mexPrintf("FangDa is send G\n");
 	}
 	else{
 		mexPrintf("FangDa is not send\n");
@@ -32,7 +71,7 @@ void begin_command(CCyUSBDevice *USBDevice_tt)
 
 	array_begin[3] = 'H';
 	if (USBDevice_tt->BulkOutEndPt->XferData(p_array_begin, len_begin) == true){
-		mexPrintf("FangDa is send H\n");
+		//mexPrintf("FangDa is send H\n");
 	}
 	else{
 		mexPrintf("FangDa is not send\n");
@@ -40,7 +79,7 @@ void begin_command(CCyUSBDevice *USBDevice_tt)
 
 	array_begin[3] = 'I';
 	if (USBDevice_tt->BulkOutEndPt->XferData(p_array_begin, len_begin) == true){
-		mexPrintf("FangDa is send I\n");
+		//mexPrintf("FangDa is send I\n");
 	}
 	else{
 		mexPrintf("FangDa is not send\n");
@@ -48,7 +87,7 @@ void begin_command(CCyUSBDevice *USBDevice_tt)
 
 	array_begin[3] = 'J';
 	if (USBDevice_tt->BulkOutEndPt->XferData(p_array_begin, len_begin) == true){
-		mexPrintf("FangDa is send J\n");
+		//mexPrintf("FangDa is send J\n");
 	}
 	else{
 		mexPrintf("FangDa is not send\n");
@@ -56,7 +95,7 @@ void begin_command(CCyUSBDevice *USBDevice_tt)
 
 	array_begin[3] = 'K';
 	if (USBDevice_tt->BulkOutEndPt->XferData(p_array_begin, len_begin) == true){
-		mexPrintf("FangDa is send K\n");
+		//mexPrintf("FangDa is send K\n");
 	}
 	else{
 		mexPrintf("FangDa is not send\n");
@@ -66,40 +105,47 @@ void begin_command(CCyUSBDevice *USBDevice_tt)
 extern "C" MEX_FUNCTION_API void mexFunction(int nlhs, mxArray *plhs[], int nrhs, mxArray*prhs[])
 {
 	CCyUSBDevice *USBDevice = new CCyUSBDevice();
-	//PUCHAR buf = new UCHAR[1024];
-	//double *x;
-	//
-	//
-	//plhs[0] = mxCreateDoubleMatrix(1, 1024, mxREAL);
-	//x = mxGetPr(plhs[0]);
-
-
-	//if (USBDevice->IsOpen()){
-	//	mexPrintf("USB has already been open\n");
-	//}
-	//else{
-	//	mexPrintf("USB has not  been open yet\n");
-	//	USBDevice->Open(0);
-		begin_command(USBDevice);
-	//}
-
-	//
-
-	//if (USBDevice->IsOpen()){
-	//	LONG len = 1024;
-	//	
-	//	USBDevice->BulkInEndPt->XferData(buf, len);
-	//	for (int i = 0; i < 1024; i++){
-	//		x[i] = buf[i];
-	//	}
-
-
-	//}
-	//else{
-	//	mexPrintf("USB is not open\n");
-	//}
-	//mexPrintf("Done\n"); 
+	int x = mxGetScalar(prhs[0]);
+	send_command(USBDevice, x);
 }
+
+//extern "C" MEX_FUNCTION_API void mexFunction(int nlhs, mxArray *plhs[], int nrhs, mxArray*prhs[])
+//{
+//	CCyUSBDevice *USBDevice = new CCyUSBDevice();
+//	//PUCHAR buf = new UCHAR[1024];
+//	//double *x;
+//	//
+//	//
+//	//plhs[0] = mxCreateDoubleMatrix(1, 1024, mxREAL);
+//	//x = mxGetPr(plhs[0]);
+//
+//
+//	//if (USBDevice->IsOpen()){
+//	//	mexPrintf("USB has already been open\n");
+//	//}
+//	//else{
+//	//	mexPrintf("USB has not  been open yet\n");
+//	//	USBDevice->Open(0);
+//	send_command(USBDevice);
+//	//}
+//
+//	//
+//
+//	//if (USBDevice->IsOpen()){
+//	//	LONG len = 1024;
+//	//	
+//	//	USBDevice->BulkInEndPt->XferData(buf, len);
+//	//	for (int i = 0; i < 1024; i++){
+//	//		x[i] = buf[i];
+//	//	}
+//
+//
+//	//}
+//	//else{
+//	//	mexPrintf("USB is not open\n");
+//	//}
+//	//mexPrintf("Done\n"); 
+//}
 
 //void usb_receive(double* x)
 ////void usb_receive()
