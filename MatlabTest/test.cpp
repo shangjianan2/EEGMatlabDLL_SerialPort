62e6a93d61505b0c1b22f46ca532b53028f309c0
unknown <1978707987@qq.com>
2018-05-24 15:39:27 +0800

    成功打开串口"COM2"，下一步准备读取数据

--- MatlabTest/test.cpp
+++ MatlabTest/test.cpp
@@ -8,7 +8,22 @@
 using namespace std;
 extern "C" MEX_FUNCTION_API void mexFunction(int nlhs, mxArray *plhs[], int nrhs, mxArray*prhs[])
 {
-	CCyUSBDevice *p = new CCyUSBDevice();
+	HANDLE m_hComm = CreateFile(TEXT("COM2"),						// communication port string (COMX)
+		GENERIC_READ | GENERIC_WRITE,	// read/write types
+		0,								// comm devices must be opened with exclusive access
+		NULL,							// no security attributes
+		OPEN_EXISTING,					// comm devices must use OPEN_EXISTING
+		FILE_FLAG_OVERLAPPED,			// Async I/O
+		0);							// template must be 0 for comm devices
+	if (m_hComm == (HANDLE)-1)
+	{
+		mexPrintf("failed to open\n");
+	}
+	else
+	{
+		mexPrintf("success to open\n");
+	}
 
-	mexPrintf("Hello World!!!\n"); 
+	CloseHandle(m_hComm);
+	 
 }
